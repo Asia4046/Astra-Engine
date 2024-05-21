c880df497483c0983bc116c3882b923b67727fa2
Asia4046 <kirito234875@gmail.com>
2024-05-21 17:57:18 +0530

    main]

---
+++ build-all.bat
@@ -0,0 +1,16 @@
+@ECHO OFF 
+REM
+
+ECHO "BUILDING EVERYTHING..."
+
+PUSHD engine
+CALL build.bat
+POPD
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+PUSHD testbed
+CALL build.bat
+POPD
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+ECHO "ALL ASSEMBLIES BUILT SUCCESSFULLY."
---
+++ testbed/build.bat
@@ -0,0 +1,21 @@
+REM BUILD SCRIPT for engine
+@ECHO OFF
+SetLocal EnableDelayedExpansion
+
+REM
+SET cFilenames=
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
+)
+
+REM
+
+SET assembly=testbed
+SET compilerFlags=-g
+REM
+SET includeFlags=-Isrc -I../engine/src/
+SET linkerFlags=-L../bin/ -lengine.lib 
+SET defines=-D_DEBUG -DKIMPORT
+
+ECHO "BUILDING [%assembly%]..."
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
