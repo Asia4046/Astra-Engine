d1460e4264d2e16927144bff0a0a861abb4999ee
Asia4046 <kirito234875@gmail.com>
2024-05-22 13:36:17 +0530

    idk

--- build-all.sh
+++ build-all.sh
@@ -22,3 +22,8 @@ echo "Error:"$ERRORLEVEL && exit
 fi 
 
 echo "ALL ASSEMBLIES BUILT SUCCESSFULLY."
+
+version=0.0.1-dev
+
+echo "Compressing Source Code and Build"
+tar -zcvf build_and_source_$version.tar.gz *
