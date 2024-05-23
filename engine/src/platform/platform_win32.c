8acde7a616c986ca469c9d75fea6165f7efaab20
Asia4046 <kirito234875@gmail.com>
2024-05-23 21:40:04 +0530

    windows_platform_layer and .vscode changes

--- engine/src/platform/platform_win32.c
+++ engine/src/platform/platform_win32.c
@@ -4,21 +4,64 @@
 
 #include <Windows.h>
 #include <windowsx.h>
+#include <stdio.h>
 
 typedef struct internal_state{
     HINSTANCE h_instance;
     HWND hwnd;
 } internal_state;
 
+LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARAM l_param);
+
 b8 platform_startup(
     platform_state* plat_state,
-    const char* application_name,
     i32 x,
     i32 y,
     i32 width,
     i32 height
-){
+) {
+    plat_state->internal_state = malloc(sizeof(internal_state));
+    internal_state* state = (internal_state *)plat_state->internal_state;
 
+    state->h_instance = GetModuleHandleA(0);
+
+    HICON icon = LoadIcon(state->h_instance, IDI_APPLICATION);
+    WNDCLASSA wc;
+    memset(&wc, 0, sizeof(wc));
+    wc.style = CS_DBLCLKS;
+    wc.lpfnWndProc = win32_process_message;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = state->h_instance;
+    wc.hIcon = icon;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = NULL;
+    wc.lpszClassName = "astra_window_class";
+
+    if(!RegisterClassA(&wc)){
+        MessageBox(0, "Window Registration Failed", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return FALSE;
+    }
+
+    u32 client_x = x;
+    u32 client_y = y;
+    u32 client_width = width;
+    u32 client_height = height;
+
+    u32 window_x = client_x;
+    u32 window_y = client_y;
+    u32 window_height = client_height;
+    u32 window_width = client_width;
+
+    u32 window_style = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION;
+    u32 window_ex_style = WS_EX_APPWINDOW;
+
+    window_style != WS_MAXIMIZEBOX;
+    window_style != WS_MINIMIZEBOX;
+    window_style != WS_THICKFRAME;
+
+    RECT border_rect = {0, 0, 0, 0};
+    AdjustWindowRectEx(&border_rect, window_style, 0, window_ex_style);
 }
 
 #endif
