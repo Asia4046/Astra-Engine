8ccbe8d5bf64a57681e7754093a39e0885639a8e
Asia4046 <kirito234875@gmail.com>
2024-05-24 22:41:21 +0530

    memory_management

--- engine/src/core/kmemory.c
+++ engine/src/core/kmemory.c
@@ -2,12 +2,35 @@
 #include "core/logger.h"
 #include "platform/platform.h"
 
+#include <string.h>
+#include <stdio.h>
+
 struct memory_stats {
 
     u64 total_allocated;
     u64 tagged_allocations[MEMORY_TAG_MAX_TAGS];
 
 };
+
+static const char* memory_tag_strings[MEMORY_TAG_MAX_TAGS] = {
+    "UNKNOWN    ",
+    "ARRAY      ",
+    "DARRAY     ",
+    "DICT       ",
+    "RING_QUEUE ",
+    "BST        ",
+    "STRING     ",
+    "APPLICATION",
+    "JOB        ",
+    "TEXTURE    ",
+    "MAT_INST   ",
+    "RENDERER   ",
+    "GAME       ",
+    "TRANSFORM  ",
+    "ENTITY     ",
+    "ENTITY_NODE",
+    "SCENE      "};
+
 
 static struct memory_stats stats;
 
@@ -56,4 +79,41 @@ KAPI void* kcopy_memory(void* dest, const void* source, u64 size)
 KAPI void* kset_memory(void* dest, i32 value, u64 size)
 {
     return platform_set_memory(dest, value, size);
+}
+
+char* get_memory_usage()
+{
+    const u64 gib = 1024 * 1024 * 1024;
+    const u64 mib = 1024 * 1204;
+    const u64 kib = 1024;
+
+    char buffer[8000] = "System Memory Use (Tagged): \n";
+    u64 offset = strlen(buffer);
+
+    for(u32 i = 0; i < MEMORY_TAG_MAX_TAGS; ++i)
+    {
+        char unit[4] = "XiB";
+        float amount = 1.0f;
+        if(stats.tagged_allocations[i] >= gib)
+        {
+            unit[0] = 'G';
+            amount = stats.tagged_allocations[i] / (float)gib;
+        }
+        else  if(stats.tagged_allocations[i] >= mib)
+        {
+            unit[0] = 'M';
+            amount = stats.tagged_allocations[i] / (float)mib;
+        }
+        else  if(stats.tagged_allocations[i] >= kib)
+        {
+            unit[0] = 'K';
+            amount = stats.tagged_allocations[i] / (float)kib;
+        }
+        else 
+        {
+            unit[0] = 'B';
+            unit[1] = 0;
+            amount = (float)stats.tagged_allocations[i];
+        }
+    }
 }
