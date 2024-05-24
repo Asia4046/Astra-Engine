cbd55618017d9e8a7fefdbf94995ed9aafab6c06
Asia4046 <kirito234875@gmail.com>
2024-05-24 17:14:24 +0530

    application_layer: initialize application_window

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -2,22 +2,20 @@
 #include <core/asserts.h>
 #include <platform/platform.h>
 #include "defines.h"
+#include <core/application.h>
 
 int main(void){
-    KFATAL("TEST MESSAGE %f", 3.14f);
-    KERROR("TEST MESSAGE %f", 3.14f);
-    KWARN("TEST MESSAGE %f", 3.14f);
-    KINFO("TEST MESSAGE %f", 3.14f);
-    KDEBUG("TEST MESSAGE %f", 3.14f);
-    KTRACE("TEST MESSAGE %f", 3.14f);
 
-    platform_state state;
-    if(platform_startup(&state, "Astra Engine Testbed", 100, 100, 1280, 720)) {
-        while(true) {
-            platform_pump_messages(&state);
-        }
-    }
-    platform_shutdown(&state);
+    application_config config;
+    config.start_pos_x = 100;
+    config.start_pos_y = 100;
+    config.start_width = 1280;
+    config.start_height = 720;
+    config.name = "ASTRA ENGINE TESTBED";
+
+    application_create(&config);
+
+    application_run();
 
     return 0;
 }
